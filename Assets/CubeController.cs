9343a095c6dd2259acbf8e78ec3a34dea266eb6c
Yo Sato <raimu_23tm@hotmail.co.jp>
2020-01-17 14:13:00 +0900

    2Dゲームアプリを作ろう　課題コミット
    課題の内容を実装。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // 効果音
+    private AudioSource myAudio;
+
+    // Use this for initialization
+    void Start()
+    {
+        //AudioSourceコンポーネントを取得
+        this.myAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        //地面とブロックが接触
+        if (collision.gameObject.tag == "CubeBlock" ||
+            collision.gameObject.tag == "Ground")
+        {
+
+            //音を鳴らす
+            myAudio.Play();
+
+        }
+
+    }
+
+}
